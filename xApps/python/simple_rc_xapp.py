e44a7ce239b3c908e842163f1d57cbb4ba43fd0a
Piotr Gawlowicz <piotr.gawlowicz@srs.io>
2025-06-11 15:48:34 +0200

    xapps: update PRB control xApp

--- xApps/python/simple_rc_xapp.py
+++ xApps/python/simple_rc_xapp.py
@@ -16,21 +16,28 @@ class MyXapp(xAppBase):
     @xAppBase.start_function
     def start(self, e2_node_id, ue_id):
         while self.running:
-            min_prb_ratio = 1
-            max_prb_ratio = 5
+            min_prb_ratio = 10
+            max_prb_ratio = 30
             current_time = datetime.datetime.now()
             print("{} Send RIC Control Request to E2 node ID: {} for UE ID: {}, PRB_min_ratio: {}, PRB_max_ratio: {}".format(current_time.strftime("%H:%M:%S"), e2_node_id, ue_id, min_prb_ratio, max_prb_ratio))
             self.e2sm_rc.control_slice_level_prb_quota(e2_node_id, ue_id, min_prb_ratio, max_prb_ratio, dedicated_prb_ratio=100, ack_request=1)
             time.sleep(5)
 
-            min_prb_ratio = 1
-            max_prb_ratio = 40
+            min_prb_ratio = 10
+            max_prb_ratio = 50
             current_time = datetime.datetime.now()
             print("{} Send RIC Control Request to E2 node ID: {} for UE ID: {}, PRB_min_ratio: {}, PRB_max_ratio: {}".format(current_time.strftime("%H:%M:%S"), e2_node_id, ue_id, min_prb_ratio, max_prb_ratio))
             self.e2sm_rc.control_slice_level_prb_quota(e2_node_id, ue_id, min_prb_ratio, max_prb_ratio, dedicated_prb_ratio=100, ack_request=1)
             time.sleep(5)
 
-            min_prb_ratio = 1
+            min_prb_ratio = 10
+            max_prb_ratio = 70
+            current_time = datetime.datetime.now()
+            print("{} Send RIC Control Request to E2 node ID: {} for UE ID: {}, PRB_min_ratio: {}, PRB_max_ratio: {}".format(current_time.strftime("%H:%M:%S"), e2_node_id, ue_id, min_prb_ratio, max_prb_ratio))
+            self.e2sm_rc.control_slice_level_prb_quota(e2_node_id, ue_id, min_prb_ratio, max_prb_ratio, dedicated_prb_ratio=100, ack_request=1)
+            time.sleep(5)
+
+            min_prb_ratio = 10
             max_prb_ratio = 100
             current_time = datetime.datetime.now()
             print("{} Send RIC Control Request to E2 node ID: {} for UE ID: {}, PRB_min_ratio: {}, PRB_max_ratio: {}".format(current_time.strftime("%H:%M:%S"), e2_node_id, ue_id, min_prb_ratio, max_prb_ratio))
@@ -45,14 +52,13 @@ if __name__ == '__main__':
     parser.add_argument("--rmr_port", type=int, default=4560, help="RMR port")
     parser.add_argument("--e2_node_id", type=str, default='gnbd_001_001_00019b_0', help="E2 Node ID")
     parser.add_argument("--ran_func_id", type=int, default=3, help="E2SM RC RAN function ID")
-    parser.add_argument("--ue_id", type=int, default=0, help="UE ID")
-
+    parser.add_argument("--gnb_cu_ue_f1ap_id", type=int, default=0, help="UE ID")
 
     args = parser.parse_args()
     config = args.config
     e2_node_id = args.e2_node_id # TODO: get available E2 nodes from SubMgr, now the id has to be given.
     ran_func_id = args.ran_func_id # TODO: get available E2 nodes from SubMgr, now the id has to be given.
-    ue_id = args.ue_id
+    ue_id = args.gnb_cu_ue_f1ap_id
 
     # Create MyXapp.
     myXapp = MyXapp(config, args.http_server_port, args.rmr_port)
